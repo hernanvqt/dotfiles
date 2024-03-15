204a9cc635d716772da8f2dbd6647967bee29a81
hernanvq <hernanvqt@gmail.com>
2024-03-15 12:52:26 +0100

    removing close buffer bd and bw

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -3,7 +3,3 @@ vim.keymap.set('n', '<leader>mg', ':CMakeGenerate<CR>', { desc = "Generate make 
 vim.keymap.set('n', '<leader>mm', ':CMakeBuild<CR>', { desc = "Build targets"})
 vim.keymap.set('n', '<leader>mr', ':CMakeRun<CR>', { desc = "Run targets"})
 
-
--- Buffers
-vim.keymap.set('n', '<leader>bd', ':bd <CR>', { desc = "Unload buffer (no changes)"})
-vim.keymap.set('n', '<leader>bdy', ':bw <CR>', { desc = "Force unload buffer"})
